34006645be62d7c05a86da36ff8e1586f36584c5
Rikun PATEL <rikunpatel@quotus.co.in>
2022-08-02 17:39:05 +0530

    new test class file pkd.java has been added

---
+++ src/test/java/com/test/actitime/PKD.java
@@ -0,0 +1,13 @@
+package com.test.actitime;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PKD 
+{
+	@Test
+	public void pkd()
+	{
+		Reporter.log("p karunakar dora" , true);
+	}
+}
